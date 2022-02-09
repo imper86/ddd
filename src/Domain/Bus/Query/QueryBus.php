0dc92fc0d517915a7ef7cfbd3b97c8fa33d10209
imper <me@imper.info>
2022-02-09 11:00:35 +0100

    added @throws InvalidResponseException annotation

--- src/Domain/Bus/Query/QueryBus.php
+++ src/Domain/Bus/Query/QueryBus.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Imper86\DDD\Domain\Bus\Query;
 
+use Imper86\DDD\Domain\Bus\Exception\InvalidResponseException;
 use Imper86\DDD\Domain\Bus\Exception\NoResponseException;
 
 interface QueryBus
@@ -12,6 +13,7 @@ interface QueryBus
      * @param Query $query
      * @return Response|null
      * @throws NoResponseException
+     * @throws InvalidResponseException
      */
     public function ask(Query $query): ?Response;
 }
