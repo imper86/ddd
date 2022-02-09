51a41ff1782e5337920bff4fdf585f4698d26de9
imper <me@imper.info>
2022-02-09 10:53:10 +0100

    added NoResponseException.php

--- src/Domain/Bus/Query/QueryBus.php
+++ src/Domain/Bus/Query/QueryBus.php
@@ -4,7 +4,14 @@ declare(strict_types=1);
 
 namespace Imper86\DDD\Domain\Bus\Query;
 
+use Imper86\DDD\Domain\Bus\Exception\NoResponseException;
+
 interface QueryBus
 {
+    /**
+     * @param Query $query
+     * @return Response|null
+     * @throws NoResponseException
+     */
     public function ask(Query $query): ?Response;
 }
